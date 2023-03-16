da9a12d17b73945e920f03da11c11a2aaa29d112
Xin Liu <livid@v2ex.com>
2023-03-15 20:38:25 -0700

    Add: /ens/list/:addr to list all ENS owned by an address

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,6 +1,7 @@
 import ENSKit
-import Vapor
+import Foundation
 import NullCodable
+import Vapor
 import web3
 
 struct Result: Codable {
@@ -12,46 +13,127 @@ struct Result: Codable {
     var juiceboxProjectID: String?
 }
 
+enum CustomError: Error {
+    case invalidURL
+    case unexpectedResponseFormat
+}
+
+func getNamesFromSubgraph(address: String) async throws -> [String] {
+    let urlString = "https://api.thegraph.com/subgraphs/name/ensdomains/ens"
+    guard let url = URL(string: urlString) else {
+        throw CustomError.invalidURL
+    }
+
+    var request = URLRequest(url: url)
+    request.httpMethod = "POST"
+    request.addValue("application/json", forHTTPHeaderField: "content-type")
+    request.addValue("https://app.ens.domains", forHTTPHeaderField: "origin")
+    request.addValue("https://app.ens.domains/", forHTTPHeaderField: "referer")
+
+    let query = """
+        query getNamesFromSubgraph($address: String!) {
+            domains(first: 1000, where: {owner: $address}) {
+                name
+            }
+        }
+        """
+
+    let requestBody: [String: Any] = [
+        "operationName": "getNamesFromSubgraph",
+        "variables": ["address": address],
+        "query": query,
+    ]
+
+    request.httpBody = try? JSONSerialization.data(withJSONObject: requestBody, options: [])
+
+    let (data, _) = try await URLSession.shared.data(for: request)
+    guard let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
+        let domainsData = json["data"] as? [String: Any],
+        let domains = domainsData["domains"] as? [[String: String]]
+    else {
+        throw CustomError.unexpectedResponseFormat
+    }
+
+    let domainNames = domains.compactMap { $0["name"] }
+    return domainNames
+}
+
 func routes(_ app: Application) throws {
     app.get { req async -> Response in
         let html = "ENS API maintained by <a href='https://planetable.xyz'>Planetable</a>"
-        
+
         var headers = HTTPHeaders()
         headers.add(name: .contentType, value: "text/html")
         return Response(status: .ok, headers: headers, body: .init(string: html))
     }
 
+    app.get("ens", "list", ":addr") { req async -> Response in
+        var output: String = "[]"
+        var headers = HTTPHeaders()
+
+        var addr: String = req.parameters.get("addr")!
+
+        // If addr is an ENS name, resolve it to an address
+        if addr.hasSuffix(".eth") {
+            let enskit = ENSKit()
+            if let resolver = try? await enskit.resolver(name: addr) {
+                if let resolvedAddress = try? await resolver.addr() {
+                    addr = "0x" + resolvedAddress.lowercased()
+                }
+            }
+        }
+
+        do {
+            let domainNames = try await getNamesFromSubgraph(address: addr.lowercased())
+            print("Domain names: \(domainNames)")
+            if let data = try? JSONEncoder().encode(domainNames),
+                let str = String(data: data, encoding: .utf8)
+            {
+                output = str
+            }
+        }
+        catch {
+            print("Error: \(error.localizedDescription)")
+        }
+
+        headers.add(name: .contentType, value: "application/json")
+        return Response(status: .ok, headers: headers, body: .init(string: output))
+    }
+
     app.get("ens", "resolve", ":query") { req async -> Response in
         let query = req.parameters.get("query")!
         let normalized = query.lowercased()
-        
+
         var address: String? = nil
         var name: String? = nil
         var displayName: String? = nil
         var avatarURLString: String? = nil
         var contentHash: String? = nil
         var juiceboxProjectID: String? = nil
-        
+
         let enskit = ENSKit()
-        
+
         // query is an address
         if normalized.hasPrefix("0x") && normalized.count == 42 {
             address = normalized
 
             // TODO: Get a checksummed address
-            
+
             if let address = address, let resolvedName = await enskit.name(addr: address) {
                 name = resolvedName
                 displayName = name
                 if let resolver = try? await enskit.resolver(name: resolvedName) {
                     if let avatar = try? await resolver.getAvatar(),
-                       let avatarURL = try? await resolver.getAvatarImageURL(from: avatar) {
+                        let avatarURL = try? await resolver.getAvatarImageURL(from: avatar)
+                    {
                         avatarURLString = avatarURL.absoluteString
                     }
                     if let contentHashURL = try? await resolver.contenthash() {
                         contentHash = contentHashURL.absoluteString
                     }
-                    if let juiceboxProjectIDString = try? await resolver.text(key: "juicebox_project_id") {
+                    if let juiceboxProjectIDString = try? await resolver.text(
+                        key: "juicebox_project_id"
+                    ) {
                         juiceboxProjectID = juiceboxProjectIDString
                     }
                 }
@@ -66,13 +148,16 @@ func routes(_ app: Application) throws {
                     displayName = name
                     if let resolver = try? await enskit.resolver(name: name) {
                         if let avatar = try? await resolver.getAvatar(),
-                           let avatarURL = try? await resolver.getAvatarImageURL(from: avatar) {
+                            let avatarURL = try? await resolver.getAvatarImageURL(from: avatar)
+                        {
                             avatarURLString = avatarURL.absoluteString
                         }
                         if let contentHashURL = try? await resolver.contenthash() {
                             contentHash = contentHashURL.absoluteString
                         }
-                        if let juiceboxProjectIDString = try? await resolver.text(key: "juicebox_project_id") {
+                        if let juiceboxProjectIDString = try? await resolver.text(
+                            key: "juicebox_project_id"
+                        ) {
                             juiceboxProjectID = juiceboxProjectIDString
                         }
                     }
@@ -80,16 +165,26 @@ func routes(_ app: Application) throws {
             }
         }
         if displayName == nil,
-           let address = address,
-           address.count == 42 {
+            let address = address,
+            address.count == 42
+        {
             displayName = String(address.prefix(5)) + "…" + String(address.suffix(4))
         }
         if avatarURLString != nil, let avatar = avatarURLString {
             if avatar.hasPrefix("ipfs://ipfs/") {
-                avatarURLString = "https://ipfs.io/ipfs/" + avatar.replacingOccurrences(of: "ipfs://ipfs/", with: "")
+                avatarURLString =
+                    "https://ipfs.io/ipfs/"
+                    + avatar.replacingOccurrences(of: "ipfs://ipfs/", with: "")
             }
         }
-        let result = Result(address: address, name: name, displayName: displayName, avatar: avatarURLString, contentHash: contentHash, juiceboxProjectID: juiceboxProjectID)
+        let result = Result(
+            address: address,
+            name: name,
+            displayName: displayName,
+            avatar: avatarURLString,
+            contentHash: contentHash,
+            juiceboxProjectID: juiceboxProjectID
+        )
         var headers = HTTPHeaders()
         let json: String = {
             do {
@@ -97,10 +192,14 @@ func routes(_ app: Application) throws {
                 encoder.outputFormatting = .prettyPrinted
                 let jsonData = try encoder.encode(result)
                 if let jsonString = String(data: jsonData, encoding: .utf8) {
-                    headers.add(name: "CDN-Cache-Control", value: "public, stale-while-revalidate=8640000, max-age=600")
+                    headers.add(
+                        name: "CDN-Cache-Control",
+                        value: "public, stale-while-revalidate=8640000, max-age=600"
+                    )
                     return jsonString
                 }
-            } catch {
+            }
+            catch {
             }
             return "{}"
         }()
